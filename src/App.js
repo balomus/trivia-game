c5fdc0efe2be6db6e78465d02337adf76ff24ef1
Stephen Mercier <balom.us@gmail.com>
2022-03-24 11:08:57 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
+  const [amount, setAmount] = useState(10);
+  const [category, setCategory] = useState(9);
+  const [difficulty, setDifficulty] = useState("easy");
+  const [type, setType] = useState("multiple");
+  const [questions, setQuestions] = useState();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleClick = async () => {
+    try{
+      const response = await fetch("https://opentdb.com/api.php?amount=" + amount + "&category=" + category + "&difficulty=" + difficulty + "&type="  + type)
+      const data = await response.json();
+      setQuestions(data);
+      console.log(data);
+      console.log("question - " + data.results[currentQuestion].question + " answer - " + data.results[currentQuestion].correct_answer);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <p>App goes here.</p>
+      <p>Sample API URL: https://opentdb.com/api.php?amount={amount}&category={category}&difficulty={difficulty}&type={type}</p>
+      <button onClick={handleClick}>Get Trivia Questions</button>
+      <div>
+        Question # {currentQuestion + 1}
+        <br></br>
+        {/* Category: {questions.results.currentQuestion.category} */}
+      </div>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
@@ -50,7 +79,7 @@ function App() {
             React Redux
           </a>
         </span>
-      </header>
+      </header> */}
     </div>
   );
 }
