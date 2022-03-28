0a94e790325d6c3e8f388708bd7496fc0abe1daa
Stephen Mercier <balom.us@gmail.com>
2022-03-28 09:37:57 -0400

    Working on adding categories drop down

--- src/features/builder/Builder.js
+++ src/features/builder/Builder.js
@@ -1,10 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAmount, setAmount } from "./builderSlice";
+import { selectAmount, selectCategory, setAmount, setCategory } from "./builderSlice";
 
 const Builder = () => {
+  const [fullCategories, setFullCategories] = useState(null);
+
+  useEffect(() => {
+    setFullCategories(async () => {
+      try {
+        const response = await fetch("https://opentdb.com/api_category.php");
+        const data = await response.json();
+        console.log(data);
+        return response.data;
+      } catch (error) {
+        return error;
+      }
+    });
+  }, []);
+
+  console.log(fullCategories);
+
   const dispatch = useDispatch();
   const amount = useSelector(selectAmount);
+  const category = useSelector(selectCategory);
+
+  let categories = [ { id: 999, name: "All Categories" } ]
+  let fullCategoriesResponse = async () => {
+    try {
+      const response = await fetch("https://opentdb.com/api_category.php");
+      const data = await response.json();
+      console.log(data);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  console.log(fullCategoriesResponse);
+
+  categories.push()
   // const [amount, setAmount] = useState(10);
   // const [category, setCategory] = useState(9);
   // const [difficulty, setDifficulty] = useState("easy");
@@ -38,6 +72,11 @@ const Builder = () => {
             <option value="30">30</option>
             <option value="40">40</option>
             <option value="50">50</option>
+          </select>
+
+          Category:
+          <select name="category" id="category-select" value={category} onChange={(e) => dispatch(setCategory(e.target.value))}>
+            <option value="All Categories">All Categories</option>
           </select>
           <p>Sample API URL: https://opentdb.com/api.php?amount={amount}&category=*&difficulty=*&type=*</p>
           <button onClick={handleClick}>Get Trivia Questions</button>
