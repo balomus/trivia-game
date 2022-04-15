68e48e0dc5193ffdfc5a0e43a51928d6bee27eed
Stephen Mercier <balom.us@gmail.com>
2022-04-15 17:14:53 -0400

    Added some state and logic for correct/incorrect answer and incrementing to next question

--- src/features/question/Question.js
+++ src/features/question/Question.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectApiResponse } from "../builder/builderSlice";
-import { selectCurrentQuestion, selectQuestionNumber } from "./questionSlice";
+import { selectCurrentQuestion, selectQuestionNumber, setCurrentQuestion, setQuestionNumber } from "./questionSlice";
 
 const Question = () => {
     
@@ -11,6 +11,9 @@ const Question = () => {
 
     const [answers, setAnswers] = useState(["sample1", "sample2", "sample3", "sample4"])
 
+    const [correctNum, setCorrectNum] = useState([0]);
+    const [incorrectNum, setIncorrectNum] = useState([0]);
+
     const dispatch = useDispatch();
 
     const decodeHtml = (html) => {
@@ -19,14 +22,7 @@ const Question = () => {
         return txt.value;
     }
 
-    const randomizeAnswers = (wrongArr, correct) => {
-        const randomNum = Math.floor(Math.random() * 4);
-        const newArr = wrongArr.splice(randomNum, 0, correct);
-        console.log("newArr is " + newArr);
-        return newArr;
-    }
-
-    useEffect(() => {
+    const randomizeAnswers = () => {
         console.log("currentQuestion changed, useEffect ran");
         const randomNum = Math.floor(Math.random() * 4);
         console.log("correct answer should be index " + randomNum);
@@ -34,16 +30,37 @@ const Question = () => {
         newArr.splice(randomNum, 0, currentQuestion.correct_answer);
         console.log(newArr);
         setAnswers(newArr);
-        // setAnswers(randomizeAnswers(currentQuestion.incorrect_answer, currentQuestion.correct_answer))
+    }
+
+    const handleClick = (e) => {
+        const answer = e.target.textContent;
+        if (answer === decodeHtml(currentQuestion.correct_answer))
+        {
+            console.log("Right answer selected");
+            setCorrectNum(Number(correctNum) + 1);
+        }
+        else
+        {
+            console.log("Wrong answer selected");
+            setIncorrectNum(Number(incorrectNum) + 1);
+        }
+        dispatch(setQuestionNumber(questionNumber + 1));
+        dispatch(setCurrentQuestion(apiResponse[questionNumber]));
+    }
+
+    useEffect(() => {
+        randomizeAnswers()
     }, [currentQuestion])
 
     return ( 
         <div>
+            <p>Correct #: {correctNum}</p>
+            <p>Incorrect #: {incorrectNum}</p>
             <h2>Question # {questionNumber} of {apiResponse.length}</h2>
             <h3>{currentQuestion.category}</h3>
             <p>{decodeHtml(currentQuestion.question)}</p>
             {answers.map((item) => {
-                return <div key={decodeHtml(item)}><button>{decodeHtml(item)}</button></div>
+                return <div key={decodeHtml(item)}><button onClick={handleClick}>{decodeHtml(item)}</button></div>
             })}
         </div>
      );
