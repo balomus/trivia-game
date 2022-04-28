657df108b5925a8bf02b1eed50af7dc1aeb0da04
Stephen Mercier <balom.us@gmail.com>
2022-04-28 16:26:33 -0400

    Fixed bug with routing when last question was answered correctly.

--- src/routes/Correct.js
+++ src/routes/Correct.js
@@ -1,12 +1,26 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { selectApiResponse } from "../features/builder/builderSlice";
+import { selectQuestionNumber } from "../features/question/questionSlice";
 
 const Correct = () => {
     const navigate = useNavigate();
 
+    const questionNumber = useSelector(selectQuestionNumber);
+    const apiResponse = useSelector(selectApiResponse);
+
     useEffect(() => {
         setTimeout(() => {
-            navigate('/question');
+            if (questionNumber > apiResponse.length)
+            {
+                navigate('/done');
+            }
+            else
+            {
+                console.log("questionNumber = " + questionNumber + " questionNumber - 2 is  " + (questionNumber - 2) +  " apiResponse.length = " + apiResponse.length);
+                navigate('/question');
+                console.log("timeout done");
+            }
         }, 3000);
     }, [])
 
